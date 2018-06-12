87b19507ac1f4d9720d54880f10205e0143aebff
INAS\Hp Pro <s.ruteeb@hotmail.com>
2018-06-12 23:57:40 +0200

    ffffff

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+    sss
                     You are logged in!
                 </div>
             </div>
